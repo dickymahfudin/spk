817ba0ec9c205c59b97206259575603b8b5567dc
dickymahfudin <dickymahfudin13@gmail.com>
2021-04-09 14:29:16 +0700

    edit dashboard

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const jsonToTable = require("../helpers/jsonToTable");
-const { criteria, link, list_location } = require("../models");
+const { criteria, list_location } = require("../models");
 
 router.get("/", async (req, res, next) => {
   const user_id = req.session.userId;
   const username = req.session.username;
-  const locations = await list_location.findAll({ where: { user_id } });
-  const criterias = await criteria.findAll({ where: { user_id } });
+  const locations = await list_location.findAll({
+    where: { user_id },
+    order: [
+      ["hasil", "ASC"],
+      ["name", "ASC"],
+    ],
+  });
+  const criterias = await criteria.getAll(user_id);
+  const rangking =
+    locations.length != 0 && locations[0].hasil ? locations[0].name : "";
   res.render("dashboard", {
     title: "Dashboard",
     username,
     location: locations.length,
     criteria: criterias.length,
+    rangking,
   });
 });
 
@@ -20,9 +29,13 @@ router.get("/table", async (req, res, next) => {
   const user_id = req.session.userId;
   const locations = await list_location.findAll({
     where: { user_id },
-    order: [["hasil", "ASC"]],
+    order: [
+      ["hasil", "ASC"],
+      ["name", "ASC"],
+    ],
     attributes: { exclude: ["createdAt", "updatedAt"] },
   });
   return res.json(jsonToTable(locations, "dataValues"));
 });
+
 module.exports = router;
